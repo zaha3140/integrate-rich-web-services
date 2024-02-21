d28f283ea6c46f5f356ad393942943a177c092e2
zaha3140 <146467052+zaha3140@users.noreply.github.com>
2024-02-21 08:19:08 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,45 @@
+        for (int i = 0; i < 10; i++) {
+        }
+
+public class RandomDataGenerator {
+        int[] data = new int[10];
+        }
+
+        for (int item : data) {
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {        console.log("Random Number: " + item);
+}
+    const data = generateRandomData();
+    return data;
+    for (let i = 0; i < 10; i++) {
+main();
+
+    }
+    const data = [];
+function main() {
+}
+function generateRandomData() {
+    });
+
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    const data = generateRandomData();
+    const data = [];
+
+
+function main() {
+}
+    return data;
+    }
+main();
+    for (let i = 0; i < 10; i++) {
+    });
+function generateRandomData() {
+        console.log("Random Number: " + item);
+}
